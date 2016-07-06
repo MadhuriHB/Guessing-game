2dc26d632840f93771ab11673a29608bd9f05395
Hackbright Student <info@hackbrightacademy.com>
2016-07-06 16:31:14 -0700

    Use try/except to check for non-integer input, make sure input is in bounds

--- game.py
+++ game.py
@@ -1,25 +1,32 @@
-# Put your code here
+    # Put your code here
 import random
+def guessing_game():
 
-name = raw_input("Hi! What\'s your name? >>> ")
-print "Hi %s! I am thinking of a number between 1 and 100. " %name
-secret_number = random.randint(1, 101)
+    name = raw_input("Hi! What\'s your name? >>> ")
+    print "Hi %s! I am thinking of a number between 1 and 100. " %name
+    secret_number = random.randint(1, 101)
 
-guess = None
-while (guess != secret_number):
-    try:
-        guess = int(raw_input("What is my number? >>> "))
-    except ValueError:
-        print "That was not a valid number!\n" 
-        continue
+    guess = None
+    while (guess != secret_number):
+        try:
+            guess = int(raw_input("What is my number? >>> "))
+        except ValueError:
+            print "That was not a valid number!\n" 
+            continue
 
 
-    if(guess<1 or guess>100):
-        print "That number is out of bounds.\n"
-    else:    
-        if(guess < secret_number):
-            print("Too low. Guess again.\n")
-        elif (guess > secret_number):
-            print ("Too high. Guess again. \n")
-    
-print ("You got it!")
+        if(guess<1 or guess>100):
+            print "That number is out of bounds.\n"
+        else:    
+            if(guess < secret_number):
+                print("Too low. Guess again.\n")
+            elif (guess > secret_number):
+                print ("Too high. Guess again. \n")
+        
+    retry = raw_input("You got it! Do you want to play again? Press y/n.")
+    if(retry == "y"):
+        guessing_game()
+    else:
+        return
+
+guessing_game()
