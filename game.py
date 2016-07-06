0135d629299569263f71bca85f27e52ddfcea0a0
Hackbright Student <info@hackbrightacademy.com>
2016-07-06 16:24:06 -0700

    Use try/except to check for non-integer input, make sure input is in bounds

--- game.py
+++ game.py
@@ -1,19 +1,25 @@
 # Put your code here
 import random
 
-too_low = 0
-too_high = 101
-guess = None
-
-name = raw_input('Hi! What\'s your name? > ')
+name = raw_input("Hi! What\'s your name? >>> ")
 print "Hi %s! I am thinking of a number between 1 and 100. " %name
 secret_number = random.randint(1, 101)
 
-while (guess!= secret_number):
-    guess = int(raw_input('What is my number? > '))
-    if (guess < secret_number):
-        print('Too low. Guess again.\n')
-    elif (guess > secret_number):
-        print ("Too high. Guess again. \n")
+guess = None
+while (guess != secret_number):
+    try:
+        guess = int(raw_input("What is my number? >>> "))
+    except ValueError:
+        print "That was not a valid number!\n" 
+        continue
+
+
+    if(guess<1 or guess>100):
+        print "That number is out of bounds.\n"
+    else:    
+        if(guess < secret_number):
+            print("Too low. Guess again.\n")
+        elif (guess > secret_number):
+            print ("Too high. Guess again. \n")
     
 print ("You got it!")
